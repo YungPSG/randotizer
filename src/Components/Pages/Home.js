776ecb7f682a97b10ceb40683ad5b8ba528d4964
YungPSG <jjohnson0994@gmail.com>
2022-03-27 23:32:13 -0600

    attempted to add routing but nah

---
+++ src/Components/Pages/Home.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react';
+
+export default function Home() {  
+
+    return (
+        <div className="home-page-wrapper">
+            You are on the home screen, nice job.
+        </div>
+    )
+}
