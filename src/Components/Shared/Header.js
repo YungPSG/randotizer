4f8825593c17f60953ea2a10c625b1ae0ecac490
YungPSG <jjohnson0994@gmail.com>
2022-03-27 23:32:26 -0600

    tried

--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -1,8 +1,11 @@
 import React, {useState} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import Home from '../Pages/Home';
+import Login from '../Pages/Login.js';
+
+import { BrowserRouter as Router, Route, Routes, useRoutes } from 'react-router-dom';
 
 
 export default function Header() {
@@ -10,17 +13,17 @@ export default function Header() {
 
         return (
              <header id="header" className="header">
-                <Navbar expand="lg" className="color-nav">
-                <Container>
-                    <Navbar.Brand href="#home" className="nav-text-solar">Randotizer</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home" className="nav-text-solar">Home</Nav.Link>
-                        <Nav.Link href="#link" className="nav-text-solar">Link</Nav.Link>
-                    </Nav>
-                    </Navbar.Collapse>
-                </Container>
+                    <Navbar expand="lg" className="color-nav">
+                    <Container>
+                        <Navbar.Brand href="#home" className="nav-text-solar">Randotizer</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home" className="nav-text-solar">Home</Nav.Link>
+                            <Nav.Link href="#login" className="nav-text-solar">Login</Nav.Link>
+                        </Nav>
+                        </Navbar.Collapse>
+                    </Container>
                 </Navbar>
             </header>
         )
--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Components/Shared/Header';
 import Login from './Components/Pages/Login';
+import Home from './Components/Pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Assets/site.css';
 
-export class App extends React.Component {
-    render() {
-        return (
-            <>
-                <Header />
-                <div className="content">
-                    <Login />
-                </div>
-            </>
-            
-        );
-    }
+export default function App (){
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [activePage, setActivePage] = useState("is-active");
+    
+    return (
+        <>
+            <Header />
+            <div className="content">
+                { isLoggedIn ? <Home /> : <Login /> }
+            </div>
+        </>
+        
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
