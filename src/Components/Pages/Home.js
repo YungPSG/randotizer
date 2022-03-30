f726da992146e26840a21b3493bf384e2e22ec34
YungPSG <jjohnson0994@gmail.com>
2022-03-29 22:02:50 -0600

    Basic Randomizer almost working

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -4,21 +4,12 @@ export default function Home() {
     const [heroList, setHeroList] = useState([]);
     const [imgList, setImgList] = useState([]);
     const [imgBaseUrl, setImgBaseUrl] = useState("https://cdn.cloudflare.steamstatic.com");
+    const [randomHeroId, setRandomHeroId] = useState(Math.floor(Math.random() * 137));
+    const [isInactive, setIsInactive] = useState('home-hero-icon');
 
-    function handleGetHeros()
-    {
-        fetch("https://api.opendota.com/api/heroes")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                console.log(result);
-                setHeroList(result);
-            },
-            (error) => {
-                console.log(error);
-            }
-        )
-    }
+    // useEffect(() => {
+    //     handleGetHeroImages();
+    // }, [])
 
     function handleGetHeroImages()
     {
@@ -26,7 +17,6 @@ export default function Home() {
         .then(res => res.json())
         .then(
             (result) => {
-                console.log(result);
                 setImgList(result);
             },
             (error) => {
@@ -35,15 +25,36 @@ export default function Home() {
         )
     }
 
+    function randomHero()
+    {
+        setRandomHeroId(Math.floor(Math.random() * 137));
+
+        for(let i = 1; i < 137; i++) {
+            if(i != randomHeroId)
+            {
+                setIsInactive('home-hero-icon-inactive');              
+            }
+        }
+    }
+
+    function clearRandom()
+    {
+        setIsInactive('home-hero-icon');  
+    }
+
     return (
         <div className="home-page-wrapper">
-            Home Page
-            <button onClick={handleGetHeroImages}>Show Heros</button>
+            <h1 className="nav-text-solar home-page-header">Home Page</h1>
+            <div className='home-page-buttons'>
+                <button className="randotizer-button" onClick={handleGetHeroImages}>Show Heros</button>
+                <button className="randotizer-button" onClick={randomHero}>Random</button>
+                <button className="randotizer-button" onClick={clearRandom}>Clear</button>
+            </div>
             <div className='hero-list-wrapper'>
                 {imgList&&
                     imgList.map((hero) => ( 
                         <div key={hero.id}> 
-                            <img src={imgBaseUrl + hero.img} title={hero.localized_name} alt={hero.localized_name} className="home-hero-icon"></img>
+                            <img id={hero.id} src={imgBaseUrl + hero.img} title={hero.localized_name} alt={hero.localized_name} className={isInactive}></img>
                         </div>    
                     ))        
                 }
